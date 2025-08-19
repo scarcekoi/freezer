8a241cf9f1732410a6907be87dc696787bfa11f0
Scarce Koi <scarcekoi@gmail.com>
2025-08-19 19:08:46 -0400

    feat: init

---
+++ src/client/java/dev/scarcekoi/freezer/client/FreezerClient.java
@@ -0,0 +1,10 @@
+package dev.scarcekoi.freezer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FreezerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/scarcekoi/freezer/Freezer.java
@@ -0,0 +1,10 @@
+package dev.scarcekoi.freezer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Freezer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
