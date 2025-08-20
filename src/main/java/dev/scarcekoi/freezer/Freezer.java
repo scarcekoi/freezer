0eb40f82b5590d7607b21014d79fc8fad1e50a4a
Scarce Koi <scarcekoi@gmail.com>
2025-08-19 20:09:20 -0400

    feat: add item with bug

--- src/main/java/dev/scarcekoi/freezer/Freezer.java
+++ src/main/java/dev/scarcekoi/freezer/Freezer.java
@@ -1,10 +1,16 @@
 package dev.scarcekoi.freezer;
 
+import dev.scarcekoi.freezer.item.Items;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Freezer implements ModInitializer {
+    public static final String MOD_ID = "freezer";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        Items.registerItems();
     }
 }
---
+++ src/main/java/dev/scarcekoi/freezer/item/Items.java
@@ -0,0 +1,25 @@
+package dev.scarcekoi.freezer.item;
+
+import dev.scarcekoi.freezer.Freezer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class Items {
+    public static final Item WAFFLE = registerItem("waffle", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Freezer.MOD_ID, name), item);
+    }
+
+    public static void registerItems() {
+        Freezer.LOGGER.info("Registering Items for " + Freezer.MOD_ID);
+    }
+
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+
+    });
+}
