df7e12d780ba87fc382e39237140162d3688f85c
Scarce Koi <scarcekoi@gmail.com>
2025-08-20 19:00:59 -0400

    feat: add waffle

---
+++ src/main/java/dev/scarcekoi/freezer/block/Blocks.java
@@ -0,0 +1,4 @@
+package dev.scarcekoi.freezer.block;
+
+public class Blocks {
+}
--- src/main/java/dev/scarcekoi/freezer/item/Items.java
+++ src/main/java/dev/scarcekoi/freezer/item/Items.java
@@ -6,10 +6,13 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class Items {
-    public static final Item WAFFLE = registerItem("waffle", new Item(new Item.Settings()));
+    public static final Item WAFFLE = registerItem("waffle", new Item(new Item.Settings()
+            .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Freezer.MOD_ID,"waffle")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Freezer.MOD_ID, name), item);
@@ -17,9 +20,8 @@ public class Items {
 
     public static void registerItems() {
         Freezer.LOGGER.info("Registering Items for " + Freezer.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(WAFFLE);
+        });
     }
-
-    ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-
-    });
 }
